95b29dfd7bdb5db045a4da1e8ff83adb5d639fc8
Shiv <sshivvss@gmail.com>
2023-01-31 12:59:15 +0530

    chore: improve token system

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::io::stdin;
-use anyhow::bail;
+#![warn(clippy::pedantic, clippy::nursery)]
 
-#[derive(Clone, Copy, Debug)]
+use anyhow::bail;
+use std::{array, fmt::Display, io::stdin};
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Marker {
     O,
     X,
@@ -9,54 +10,102 @@ enum Marker {
 }
 
 #[derive(Debug)]
-struct Position(
-    Marker,
-    Marker,
-    Marker,
-    Marker,
-    Marker,
-    Marker,
-    Marker,
-    Marker,
-    Marker,
-);
+struct Position([Marker; 9]);
 
 impl Position {
-    fn new() -> Self {
-        Self::_repeat(Marker::Empty)
-    }
-
-    fn _repeat(n: Marker) -> Self {
-        Self(n, n, n, n, n, n, n, n, n)
+    const fn new() -> Self {
+        Self([Marker::Empty; 9])
     }
 
     fn from(notation: &str) -> anyhow::Result<Self> {
         let notation = notation.split('|').collect::<Vec<&str>>();
-        let mut cnotation = vec![];
+        let mut tokens = vec![];
 
         if notation.len() != 9 {
             bail!("Failed to process notation");
         }
 
         for c in notation {
-            cnotation.push(match c {
+            tokens.push(match c {
                 "" => Marker::Empty,
                 "x" => Marker::X,
                 "o" => Marker::O,
                 _ => bail!("Invalid character"),
-            })
+            });
         }
-        Ok(Self(
-            cnotation[0],
-            cnotation[1],
-            cnotation[2],
-            cnotation[3],
-            cnotation[4],
-            cnotation[5],
-            cnotation[6],
-            cnotation[7],
-            cnotation[8],
-        ))
+        Ok(Self(array::from_fn(|i| tokens[i])))
+    }
+
+    fn winning(&self) -> Option<Marker> {
+        let winning_positions = vec![
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for pos in winning_positions {
+            if self.0[pos[0]] == self.0[pos[1]]
+                && self.0[pos[1]] == self.0[pos[2]]
+                && self.0[pos[0]] != Marker::Empty
+            {
+                return Some(self.0[pos[0]]);
+            }
+        }
+
+        None
+    }
+
+    fn player(&self) -> Marker {
+        let x = self.0.iter().filter(|&x| *x == Marker::X);
+        let o = self.0.iter().filter(|&o| *o == Marker::O);
+
+        if x.count() <= o.count() {
+            Marker::X
+        } else {
+            Marker::O
+        }
+    }
+
+    fn draw(&self) -> bool {
+        let empty = self.0.iter().filter(|&e| *e == Marker::Empty);
+
+        if self.winning().is_some() && empty.count() == 0 {
+            return true;
+        }
+        false
+    }
+}
+
+impl Display for Marker {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::O => "o",
+                Self::X => "x",
+                Self::Empty => " ",
+            }
+        )
+    }
+}
+
+impl Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let [m0, m1, m2, m3, m4, m5, m6, m7, m8] = &self.0;
+        write!(
+            f,
+            r#"
+{m0}|{m1}|{m2}
+{m3}|{m4}|{m5}
+{m6}|{m7}|{m8}        
+        "#
+        )
     }
 }
 
@@ -85,10 +134,18 @@ fn main() {
                 split.next();
                 if let Ok(res) = Position::from(split.next().unwrap()) {
                     position = res;
+                    println!("{:?}", position);
                 }
             } else if command.starts_with("go") {
-                dbg!(position);
-                unimplemented!("entry")
+                println!("{}", position);
+                if let Some(marker) = position.winning() {
+                    println!("winner: {:?}", marker);
+                    continue;
+                } else if position.draw() {
+                    println!("draw");
+                    continue;
+                }
+                println!("plays: {:?}", position.player());
             }
         }
     }
